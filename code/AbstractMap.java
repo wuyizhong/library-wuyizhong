d88f19b310e12ae9be38cda7c8abf7acaa2bfb18
乌衣冢 <wuyizhong.0905@gmail.com>
2015-02-02 02:56:35 +0800

    Map的一些有点意思的代码

---
+++ code/AbstractMap.java
@@ -0,0 +1,25 @@
+﻿ 	// HashMap的remove()方法只是去掉了该Key的值
+ 	public V remove(Object key) {
+		Iterator<Entry<K, V>> i = entrySet().iterator();
+		Entry<K, V> correctEntry = null;
+		if (key == null) {
+			while (correctEntry == null && i.hasNext()) {
+				Entry<K, V> e = i.next();
+				if (e.getKey() == null) 
+					correctEntry = e;
+			}
+		} else {
+			while (correctEntry == null && i.hasNext()) {
+				Entry<K, V> e = i.next();
+				if (key.equals(e.getKey()))
+					correctEntry = e;
+			}
+		}
+		
+		V oldValue = null;
+		if (correctEntry != null) {
+			oldValue = correctEntry.getValue();
+			i.remove();
+		}
+		return oldValue;
+	}
